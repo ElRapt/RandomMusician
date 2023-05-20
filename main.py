365b754f77eaf694593792d86f32f18bbe41c1a3
Lucas Denis <lucas.denis@etu.uca.fr>
2023-05-20 11:42:41 +0200

    Large number of improvements

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import random
-import pyaudio
 import numpy as np
-import math
 import sounddevice as sd
+from pydub import AudioSegment
+from pydub.playback import play
+import os
 
+# Constants
+SAMPLE_RATE = 44100  
+DURATION = 0.3
+VOLUME = 0.2
 
-sample_rate = 44100  
-duration = 0.3
-volume = 0.2
-
-echelle_majeure = {
+MAJOR_SCALE = {
     'Do': 261.63,
     'Ré': 293.66,
     'Mi': 329.63,
@@ -19,23 +20,49 @@ echelle_majeure = {
     'Si': 493.88
 }
 
-sequence_notes = []
-for _ in range(20):
-    note = random.choice(list(echelle_majeure.keys()))
-    sequence_notes.append(note)
-
-print(sequence_notes)
-
+PIANO_NOTES = {
+    'Do': 'piano_notes/c1.wav',
+    'Ré': 'piano_notes/d1.wav',
+    'Mi': 'piano_notes/e1.wav',
+    'Fa': 'piano_notes/f1.wav',
+    'Sol': 'piano_notes/g1.wav',
+    'La': 'piano_notes/a1.wav',
+    'Si': 'piano_notes/b1.wav'
+}
 
 def generate_tone(frequency):
-    samples = np.arange(int(duration * sample_rate))
-    signal = volume * np.sin(2 * np.pi * frequency * samples / sample_rate)
+    """Generate a sinusoidal tone."""
+    samples = np.arange(int(DURATION * SAMPLE_RATE))
+    signal = VOLUME * np.sin(2 * np.pi * frequency * samples / SAMPLE_RATE)
     return signal.astype(np.float32)
 
-def jouer_note(frequency):
+def play_tone(frequency):
+    """Play a tone of a given frequency."""
     signal = generate_tone(frequency)
-    sd.play(signal, sample_rate)
+    sd.play(signal, SAMPLE_RATE)
     sd.wait()
 
-for note in sequence_notes:
-    jouer_note (echelle_majeure[note])
+def create_sequence(num_notes):
+    """Create a random sequence of notes."""
+    sequence = []
+    for _ in range(num_notes):
+        note = random.choice(list(MAJOR_SCALE.keys()))
+        sequence.append(note)
+    return sequence
+
+def play_note_sequence(sequence):
+    """Play a sequence of notes."""
+    for note in sequence:
+        file_path = os.path.join(os.getcwd(), PIANO_NOTES[note])
+        if not os.path.isfile(file_path):
+            print(f"File not found: {file_path}")
+            continue
+        try:
+            piano_note = AudioSegment.from_wav(file_path)
+            play(piano_note)
+        except Exception as e:
+            print(f"Failed to play note: {e}")
+
+sequence_notes = create_sequence(5)
+print(sequence_notes)
+play_note_sequence(sequence_notes)
